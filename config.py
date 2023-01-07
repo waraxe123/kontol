a40ea3b2cac3d5dd0a50b3fc6105c7b400e29ecb
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-01-07 14:04:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgresql://qbrbrjnv:tqVmHud0XLveTkUxtlth4JKh-9oUnPcB@ruby.db.elephantsql.com/qbrbrjnv")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001517202331"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001875475504"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001833178908"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
