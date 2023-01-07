7fbe8f6f15358a3065e3694eb917c72b328c79cc
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2023-01-07 14:31:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgresql://tzdhivuu:pFOWwuOP3bJofbf4dTLzo5DdI0MrKFBx@hansken.db.elephantsql.com/tzdhivuu")
+DB_URI = os.environ.get("DATABASE_URL", "postgresql://kfiddfbv:ngq5Ui0Wwymr8pnFnLYCprMiC__lapYG@hansken.db.elephantsql.com/kfiddfbv")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001875475504"))
